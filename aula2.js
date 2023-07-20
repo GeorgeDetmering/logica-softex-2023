d11a72b6a5d57f1b49cbf60fd8b0734ee1323ba9
George Detmering <georgedetmering@gmail.com>
2023-07-20 23:31:30 +0000

    exercicio 1

---
+++ aula2.js
@@ -0,0 +1,12 @@
+
+var nomes = ['George', 'Junior', 'Fabio'];
+
+let contador = 1;
+for (nome of nomes) {
+  console.log(contador + ": " + nome)
+  contador++
+}
+for (var i = 0; i < nomes.lenght; i++) {
+  console.log(i + 1) + " " + nomes[i]
+}
+
